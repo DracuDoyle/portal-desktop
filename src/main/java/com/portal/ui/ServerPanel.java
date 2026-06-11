a4752d8e6e7954beb71aaf54f5a61f426df2ea1b
DracuDoyle <bayron.fregoso@gmail.com>
2026-06-10 19:00:53 -0600

    feat: server panel created

--- src/main/java/com/portal/ui/ServerPanel.java
+++ src/main/java/com/portal/ui/ServerPanel.java
@@ -1,5 +1,36 @@
 package com.portal.ui;
 
-public class ServerPanel {
-    
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+
+
+
+public class ServerPanel extends JPanel {
+
+
+    private JLabel statusLabel;
+    private JLabel ipLabel;
+    private JButton toggleButton;
+
+
+    public ServerPanel() {
+        setLayout( new FlowLayout( FlowLayout.LEFT ) );
+        this.initComponents();
+    }
+
+
+    private void initComponents() {
+
+        statusLabel = new JLabel( "Servidor detenido" );
+        ipLabel = new JLabel( "IP: --" );
+        toggleButton = new JButton( "Iniciar" );
+
+        add( statusLabel );
+        add( ipLabel );
+        add( toggleButton );
+
+    }
+
 }
