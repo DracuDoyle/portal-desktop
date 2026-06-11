a2b46c56054756bc74b68fd51e370b2f012392bd
DracuDoyle <bayron.fregoso@gmail.com>
2026-06-10 19:33:41 -0600

    feat: receive panel created

--- src/main/java/com/portal/ui/ReceivePanel.java
+++ src/main/java/com/portal/ui/ReceivePanel.java
@@ -1,5 +1,49 @@
 package com.portal.ui;
 
-public class ReceivePanel {
-    
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+
+
+public class ReceivePanel extends JPanel {
+
+
+    private JLabel statusLabel;
+    private JLabel subLabel;
+    private JButton acceptButton;
+    private JButton rejectButton;
+
+
+    public ReceivePanel() {
+        setLayout( new BorderLayout() );
+        this.initComponents();
+    }
+
+
+    private void initComponents() {
+
+        statusLabel = new JLabel( "Esperando transferencia...", SwingConstants.CENTER );
+        subLabel = new JLabel( "El celular debe conectarse a esta IP", SwingConstants.CENTER );
+
+        acceptButton = new JButton( "Aceptar" );
+        rejectButton = new JButton( "Rechazar" );
+        acceptButton.setEnabled( false );
+        rejectButton.setEnabled( false );
+
+        JPanel buttonPanel = new JPanel( new FlowLayout( FlowLayout.CENTER ) );
+        buttonPanel.add( acceptButton );
+        buttonPanel.add( rejectButton );
+
+        add( statusLabel, BorderLayout.NORTH );
+        add( subLabel, BorderLayout.CENTER );
+        add( buttonPanel, BorderLayout.SOUTH );
+
+    }
+
+
 }
