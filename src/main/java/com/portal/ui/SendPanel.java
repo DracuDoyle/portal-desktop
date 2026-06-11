a6273d84a28f5c57e6dd16dedd9ed59fc87c7dd1
DracuDoyle <bayron.fregoso@gmail.com>
2026-06-10 19:56:43 -0600

    feat: send panel created

--- src/main/java/com/portal/ui/SendPanel.java
+++ src/main/java/com/portal/ui/SendPanel.java
@@ -1,5 +1,45 @@
 package com.portal.ui;
 
-public class SendPanel {
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+
+
+
+public class SendPanel extends JPanel {
+
+
+    private JLabel ipLabel;
+    private JTextField ipField;
+    private JLabel fileLabel;
+    private JButton chooseButton;
+    private JButton sendButton;
+
+
+    public SendPanel() {
+        setLayout( new GridLayout( 2, 3, 5, 5 ) );
+        this.initComponents();
+    }
+
+
+    private void initComponents() {
+
+        ipLabel = new JLabel( "IP del celular" );
+        ipField = new JTextField( "192.168.100.X" );
+        fileLabel = new JLabel( "Ningún archivo seleccionado" );
+        chooseButton = new JButton( "Elegir archivo" );
+        sendButton = new JButton( "Enviar" );
+
+        add( ipLabel );
+        add( ipField );
+        add( new JLabel() );
+        add( fileLabel );
+        add( chooseButton );
+        add( sendButton );
     
+    }
+
+
 }
